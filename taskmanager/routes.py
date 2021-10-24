420b9ee40cc5810e5048a20c9dff80a878afc236
Koko-66 <k.cisek@gmail.com>
2021-10-24 22:40:35 +0000

    Add models and routes files.

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -1,8 +1,10 @@
 from flask import render_template
 from taskmanager import app, db
+from taskmanager.models import Category, Task
 
 
 """Set route function for base.html"""
 @app.route("/")
 def home():
     return render_template("base.html")
+
