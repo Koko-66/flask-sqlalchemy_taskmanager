712105cb393f956b11cec87bc0e67fb0b05e2dcc
Koko-66 <k.cisek@gmail.com>
2021-10-27 21:28:31 +0000

    Add functionality for adding task.

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,3 +1,11 @@
   $(document).ready(function(){
+    // sidenav initialisation
     $('.sidenav').sidenav();
+    // datepicker initialisation
+    $('.datepicker').datepicker({
+      format: "dd mmmm, yyyy",
+      i18in: {done: "Select"}
+    });
+    // select initialization
+    $('select').formSelect();
   });
